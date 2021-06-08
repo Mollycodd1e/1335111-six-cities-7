aa5d75929d7acb2103bb22b4f01765175f169686
Mollycodd1e <selik7@mail.ru>
2021-06-08 11:46:13 +0300

    Изменяет путь к компоненту not-found-page

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -3,7 +3,7 @@ import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import {AppRoute} from '../../const.js';
 import Main from '../main/main.jsx';
 import Favorites from '../favorites/favorites.jsx';
-import NotFoundPage from '../notFoundPage/not-found-page.jsx';
+import NotFoundPage from '../not-found-page/not-found-page.jsx';
 import SignIn from '../sign-in/sign-in.jsx';
 import Room from '../room/room.jsx';
 import PropTypes from 'prop-types';
