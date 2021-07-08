04be6274997e75a470d8e073a56350bb6201df58
Mollycodd1e <selik7@mail.ru>
2021-07-08 10:21:40 +0300

    Удаляет предыдущие предложения на карте

--- project/src/components/map/map.jsx
+++ project/src/components/map/map.jsx
@@ -2,13 +2,27 @@ import React, {useRef} from 'react';
 import useMap from './use-map.jsx';
 import offersListProp from '../offersList/offers-list.prop';
 import offerProp from '../offersList/offer.prop.jsx';
+import leaflet from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 
 function Map(props) {
   const {offers, activeOffer} = props;
 
   const mapRef = useRef(null);
 
-  useMap(mapRef, offers, activeOffer);
+  const defaultIcon = leaflet.icon({
+    iconUrl: 'img/pin.svg',
+    iconSize: [27, 39],
+    iconAnchor: [15, 30],
+  });
+
+  const activeIcon = leaflet.icon({
+    iconUrl: 'img/pin-active.svg',
+    iconSize: [27, 39],
+    iconAnchor: [15, 30],
+  });
+
+  useMap(mapRef, offers, activeOffer, defaultIcon, activeIcon);
 
   return <section className="cities__map map" id="map" ref={mapRef} style={{height:'100%'}}></section>;
 }
