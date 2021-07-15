0928874743060c3872e9606ed44445f209de6474
Mollycodd1e <selik7@mail.ru>
2021-07-15 06:56:09 +0300

    Исправляет работу сортировки

--- project/src/components/main/main.jsx
+++ project/src/components/main/main.jsx
@@ -4,7 +4,7 @@ import Header from '../header/header.jsx';
 import Map from '../map/map.jsx';
 import OffersList from '../offersList/offers-list.jsx';
 import CitiesList from '../cities-list/cities-list.jsx';
-import {getOffersListByCity} from '../../utils.js';
+import {getOffersListByCity, getSortedOffers} from '../../utils.js';
 import SortList from '../sort-list/sort-list.jsx';
 import {getActiveCity, getSortType} from '../../store/changer/selectors.js';
 import {getOffers} from '../../store/data/selectors.js';
@@ -15,6 +15,7 @@ function Main() {
   const offers = useSelector(getOffers);
   const offersListByCity = getOffersListByCity(offers, activeCity);
   const sortType = useSelector(getSortType);
+  const sortedOffers = getSortedOffers(sortType,offersListByCity);
 
   const [activeOffer, setActiveOffer] = useState();
 
@@ -34,18 +35,18 @@ function Main() {
           </section>
         </div>
         <div className="cities">
-          {offersListByCity.length > 0 ?
+          {sortedOffers.length > 0 ?
             <div className="cities__places-container container">
               <section className="cities__places places">
                 <h2 className="visually-hidden">Places</h2>
-                <b className="places__found">{offersListByCity.length} places to stay in {activeCity}</b>
-                <SortList sortType={sortType} />
+                <b className="places__found">{sortedOffers.length} places to stay in {activeCity}</b>
+                <SortList />
                 <div className="cities__places-list places__list tabs__content">
-                  <OffersList offers={offersListByCity} isNearby={false} onOfferHover={handleOfferHover}/>
+                  <OffersList offers={sortedOffers} isNearby={false} onOfferHover={handleOfferHover}/>
                 </div>
               </section>
               <div className="cities__right-section">
-                <Map offers={offersListByCity} activeCity={activeCity} activeOffer={activeOffer}/>
+                <Map offers={sortedOffers} activeCity={activeCity} activeOffer={activeOffer}/>
               </div>
             </div>
             : <MainEmpty />}
